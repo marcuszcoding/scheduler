3d7d823517580e627cbbdcbbd328c62ae1aab1da
marcuszcoding <marcuszcoding@gmail.com>
2023-03-24 12:31:33 -0600

    add interviewerListItem and start interviwerList

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -3,14 +3,19 @@ import classNames from "classnames";
 import "components/InterviewerListItem.scss"
 
 export default function InterviewerListItem(props) {
+  
+  const interviewerListItemClass = classNames("interviewers__item", {
+    "interviewers__item--selected": props.selected
+  });
+  
   return (
-    <li className="interviewers__item">
-    <img
-      className="interviewers__item-image"
-      src="https://i.imgur.com/LpaY82x.png"
-      alt="Sylvia Palmer"
-    />
-      Sylvia Palmer
+    <li className={interviewerListItemClass} onClick={props.setInterviewer}>
+      <img
+        className="interviewers__item-image"
+        src={props.avatar}
+        alt={props.name}
+      />
+      {props.selected && props.name}
     </li>
-  )
+  );
 }
