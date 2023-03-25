7ff58e8412f5ae3442fd9cf3f2009b5a722eadfb
marcuszcoding <marcuszcoding@gmail.com>
2023-03-24 18:14:03 -0600

    fix wrong story editting

--- stories/index.js
+++ stories/index.js
@@ -38,11 +38,7 @@ storiesOf("Button", module)
   .add("Selected", () => <DayListItem name="Monday" spots={5} selected />) 
   .add("Full", () => <DayListItem name="Monday" spots={0} />)
   .add("Clickable", () => (
-    <InterviewerListItem
-      name={interviewer.name}
-      avatar={interviewer.avatar}
-      setInterviewer={() => action("setInterviewer")(interviewer.id)}
-    />
+    <DayListItem name="Tuesday" setDay={action("setDay")} spots={5} /> // action() allows us to create a callback that appears in the actions panel when clicked
   ));
 
   /// Day List ///
