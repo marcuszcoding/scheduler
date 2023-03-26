99deca1d665375da681390729a66acc679b42561
marcuszcoding <marcuszcoding@gmail.com>
2023-03-25 21:10:47 -0600

    add confirm component and sotry

--- stories/index.js
+++ stories/index.js
@@ -15,6 +15,7 @@ import Appointment from "components/Appointment/index";
 import Header from "components/Appointment/Header"
 import Empty from "components/Appointment/Empty"
 import Show from "components/Appointment/Show"
+import Confirm from "components/Appointment/Confirm"
 /// Button ///
 
 storiesOf("Button", module)
@@ -157,5 +158,4 @@ storiesOf("InterviewerList", module)
     .add("Header", () => <Header time="12pm" />)
     .add("Empty", () => <Empty onAdd={action("onAdd")}/>)
     .add("Show", () => <Show student="Lydia Miller-Jones" interviewer={interviewer.name} onEdit={action("onEdit")} onDelete={action("onDelete")} />)
-
-  /// Show Component Story ///
+    .add("Confirm", () => <Confirm message="Delete the appointment?" onConfirm={action("onConfirm")} onCancel={action("onCancel")} />)
