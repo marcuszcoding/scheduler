d2406394b94b154f11464d44dbd706f8bec6071c
marcuszcoding <marcuszcoding@gmail.com>
2023-03-26 11:20:54 -0600

    add Form component and story but no functions made

--- stories/index.js
+++ stories/index.js
@@ -18,6 +18,7 @@ import Show from "components/Appointment/Show"
 import Confirm from "components/Appointment/Confirm"
 import Status from "components/Appointment/Status"
 import Error from "components/Appointment/Error"
+import Form from "components/Appointment/Form"
 /// Button ///
 
 storiesOf("Button", module)
@@ -163,3 +164,5 @@ storiesOf("InterviewerList", module)
     .add("Confirm", () => <Confirm message="Delete the appointment?" onConfirm={action("onConfirm")} onCancel={action("onCancel")} />)
     .add("Status", () => <Status message="Deleting"/>)
     .add("Error", () => <Error message="Could not delete appointment" onClose={action("onClose")}/>)
+    .add("Edit", () => <Form student="Marcus Zimmer" interviewer={1} interviewers={interviewers} onSave={action("onSave")} onCancel={action("onCancel")}/>)
+    .add("Create", () => <Form interviewers={interviewers} onSave={action("onSave")} onCancel={action("onCancel")}/>)
