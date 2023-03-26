e391075ee94521df1c6cfc5c9fd12ea2eae7fdfa
marcuszcoding <marcuszcoding@gmail.com>
2023-03-25 21:15:40 -0600

    add status component and story

--- stories/index.js
+++ stories/index.js
@@ -16,6 +16,7 @@ import Header from "components/Appointment/Header"
 import Empty from "components/Appointment/Empty"
 import Show from "components/Appointment/Show"
 import Confirm from "components/Appointment/Confirm"
+import Status from "components/Appointment/Status"
 /// Button ///
 
 storiesOf("Button", module)
@@ -159,3 +160,4 @@ storiesOf("InterviewerList", module)
     .add("Empty", () => <Empty onAdd={action("onAdd")}/>)
     .add("Show", () => <Show student="Lydia Miller-Jones" interviewer={interviewer.name} onEdit={action("onEdit")} onDelete={action("onDelete")} />)
     .add("Confirm", () => <Confirm message="Delete the appointment?" onConfirm={action("onConfirm")} onCancel={action("onCancel")} />)
+    .add("Status", () => <Status message="Deleting"/>)
