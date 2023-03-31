4ca79e167f73a61e004f4e5969625f5a30d3a06e
marcuszcoding <marcuszcoding@gmail.com>
2023-03-31 14:44:41 -0600

    managing setDay/setDays with new setState const

--- src/components/Application.js
+++ src/components/Application.js
@@ -45,9 +45,12 @@ const appointments = {
 };
 
 export default function Application(props) {
-  const [day, setDay] = useState("Monday");
-
-  const [days, setDays] = useState([]);
+  const setDay = (day) => setState({ ...state, day });
+  const setDays = (days) => setState((prev) => ({ ...prev, days }));
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+  });
 
   useEffect(() => {
     const urlDays = `http://localhost:8001/api/days`;
@@ -71,7 +74,7 @@ export default function Application(props) {
         />
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
-          <DayList days={days} value={day} onChange={setDay} />
+          <DayList days={state.days} value={state.day} onChange={setDay} />
         </nav>
         <img
           className="sidebar__lhl sidebar--centered"
